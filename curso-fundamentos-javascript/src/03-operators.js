2878f4db63ae8da425c63aca2df7e19a7e4f355c
ana karen rivas roman <karen-120797@hotmail.com>
2026-03-05 18:15:40 -0600

    ejercicios

---
+++ curso-fundamentos-javascript/src/03-operators.js
@@ -0,0 +1,62 @@
+//operadores aritmeticos
+//suma
+const suma = 5+3;
+console.log(suma);
+
+//resta
+const resta = 5-3;
+console.log(resta);
+
+//multiplicacion
+const multiplicacion = 5 *3;
+console.log(multiplicacion);
+
+//division
+const division = 6/3;
+console.log(division);
+
+//modulo
+const modulo = 5%2;
+console.log(modulo);
+
+//exponenciacion
+const potencia = 2**3;
+console.log(potencia);
+
+//Operadores de asignacion
+
+//asignacion con suma
+let a = 5;
+a += 3;
+console.log(a);
+
+//asignacion con resta
+let b = 10;
+b -= 4;
+console.log(b);
+
+//asignacion con multiplicacioon
+let c = 6;
+c *= 2;
+console.log(c);
+
+//asignacion con division
+let d = 15;
+d /= 3;
+console.log(d);
+
+//Operadores de incremento y decremento
+let contador = 0;
+contador ++; //aumenta 1
+console.log(contador);
+
+contador --;//decrementa 1
+console.log(contador);
+
+//infinity
+console.log(1/0);
+console.log(-1/0);
+
+//NaN
+console.log(0/0);
+console.log('Hola' *2);
